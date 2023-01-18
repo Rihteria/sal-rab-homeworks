0b1c07a733960fcae394bd0223f9effafaffeedd
Rihteria <priestvel@yandex.ru>
2023-01-18 21:18:26 +0300

    3st homework

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,18 +1,21 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
- let message;
- message = productName;
- message += ' ';
- message = message + "За";
- message += ' ';
- message = productPrice;
- message += ' ';
- message = message + "Теперь в корзине";
- console.log (message);
+    let message;
+    message = productName
+    message += ' ';
+    message += 'за';
+    message += ' ';
+    message += productPrice;
+    message += ' ';
+    message += 'теперь в корзине!';
+    
+    console.log (message);
+
 
     // Конец решения задания №1.1.
 
@@ -23,11 +26,6 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
-
     let newValue; 
     newValue = oldValue;
     newValue += 1;
@@ -40,11 +38,10 @@ function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
 
-    // Задание №1.3. Увеличение суммы в корзине
-
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
+    let newSum = oldSum + difference;
+    let newSumText = newSum + '₽';
+    newSumText = `${newSum} ₽`;
+   
 
     // Конец решения задания №1.3.
 
